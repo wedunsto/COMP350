0b750bb4371b26d4e91f5f4fdceb54579dfca175
William <wedunsto@aggies.ncat.edu>
2020-09-28 13:38:36 +0000

    Imported file values to vector

--- 5.cpp
+++ 5.cpp
@@ -3,8 +3,9 @@
 #include<pthread.h>
 #include<fstream>
 #include<vector>
+#include<sstream>
 using namespace std;
-//Use string stream to get integers and add to vector
+
 void populateThreadArray(pthread_t *threads, int size){
 	for(int i=0;i<size;i++){
 		pthread_t thread;
@@ -18,10 +19,10 @@ void populateValuesVector(vector<int> &fileValues, string fileName){
 	ifstream readFile(fileName);
 	string temp;
 	while(getline(readFile,temp)){
-		char *token=strtok(temp," ");
-		while(token!=NULL){
-			fileValues.push_back(stoi(token));
-			token=strtok(NULL," ");
+		stringstream stringValues(temp);
+		string words;
+		while(stringValues>>words){
+			fileValues.push_back(stoi(words));
 		}
 	}
 }
@@ -37,6 +38,5 @@ int main(int argc, char* argv[]){
 	string inputFileName(argv[2]);
 	vector<int> values;
 	populateValuesVector(values,inputFileName);
-	cout<<"\n"<<values.size()<<endl;
 	return 0;
 }
