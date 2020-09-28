b962b1f93ddcdcb3be9ea9c65c07776afa76e8cf
William <wedunsto@aggies.ncat.edu>
2020-09-28 16:04:47 +0000

    Added more to 5.cpp

--- 5.cpp
+++ 5.cpp
@@ -1,3 +1,11 @@
+/*
+ *William Dunston
+ *COMP 350: Section 1
+ *September 28, 2020
+ *Program assignment 4: Perform math using a dynamic count
+ *of threads
+ */
+
 #include<iostream>
 #include<string.h>
 #include<pthread.h>
@@ -6,17 +14,19 @@
 #include<sstream>
 using namespace std;
 
+int slice=0;//Keep track of list sections
+//https://www.geeksforgeeks.org/sum-array-using-pthreads/
 void *printID(void *arg){
-	int *myID=(int *)arg;
-	cout<<*myID<<endl;
+	cout<<pthread_self()<<"\n";
+	pthread_exit(NULL);
+	return NULL;
 }
 
-void populateThreadArray(pthread_t *threads, int size){
-	for(int i=0;i<size;i++){
-		cout<<i<<endl;
-	}
+void *calculateThreadSums(void *arg){
+	int threadSection=slice;
+	return NULL;
 }
-
+//Function to add file integers to a vector
 void populateValuesVector(vector<int> &fileValues, string fileName){
 	ifstream readFile(fileName);
 	string temp;
@@ -31,14 +41,20 @@ void populateValuesVector(vector<int> &fileValues, string fileName){
 
 //Main method accepts inputs from the command line
 int main(int argc, char* argv[]){
-	if(argc!=3){
+	int numberOfThreads=atoi(argv[1]);//Stores the number of threads
+	if(argc!=3){//Handles too many or too little arguments
 		cout<<"There are too many or too little arguments"<<endl;
 		return 0;
 	}
-	pthread_t threads[atoi(argv[1])];
-	populateThreadArray(threads,atoi(argv[1]));
-	string inputFileName(argv[2]);
-	vector<int> values;
-	populateValuesVector(values,inputFileName);
+	int finalSum=0;//Store the final sum of thread sums
+	pthread_t threads[numberOfThreads];//Create an array of threads
+	int threadSums[numberOfThreads];//Array to store the thread sums
+	string inputFileName(argv[2]);//Gets the file name
+	vector<int> values;//Create a vector to store the file values
+	
+	for(int i=0;i<numberOfThreads;i++){//Populate thread array
+		pthread_create(&threads[i],NULL,printID,NULL);
+	}
+	populateValuesVector(values,inputFileName);//Add file values to vector
 	return 0;
 }
